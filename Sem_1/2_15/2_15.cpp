6d105af997bcf4450a411220feab95bc1dd11cb4
6o9puH <151661159+6o9puH@users.noreply.github.com>
2023-11-29 19:35:20 +0500

    Add files via upload

---
+++ Sem_1/2_15/2_15.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int s;
+	int f = 1;
+
+	cin >> s;
+
+	while (s != 0)
+	{
+		if (s % 10 != 5)
+		{
+			f = 1;
+		}
+		s /= 10;
+	}
+
+	if (f == 1)
+	{
+		cout << "true";
+	}
+	else
+	{
+		cout << "false";
+	}
+
+	return 0;
+}
