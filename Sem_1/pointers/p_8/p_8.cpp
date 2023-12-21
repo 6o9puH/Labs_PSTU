7a93b72ef9e247922de3f7f05297ff138d2e5558
6o9puH <151661159+6o9puH@users.noreply.github.com>
2023-12-22 01:22:49 +0500

    Add files via upload

---
+++ Sem_1/pointers/p_8/p_8.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int cA, ca, others, total;
+    cA = ca = others = total = 0;
+    string s;
+    char A, a;
+
+    cin >> s;
+    cin >> A;
+    cin >> a;
+
+    const char* ptr = s.c_str();
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (*ptr == A)
+        { 
+            cA++; 
+        }
+        else if (*ptr == a)
+        { 
+            ca++;
+        }
+        else 
+        { 
+            others++;
+        }
+
+        ptr++;
+
+    }
+
+    total = cA + ca + others;
+
+    cout << A << " : " << cA << endl << a << " : " << ca << endl << "others : " << others << endl << "total : " << total;
+
+    return 0;
+}
