ba663d3d443d81394da2fad034ad5b96dc56bba4
6o9puH <151661159+6o9puH@users.noreply.github.com>
2023-12-22 01:22:15 +0500

    Add files via upload

---
+++ Sem_1/nonlinear_equations/iteration/iteration.cpp
@@ -0,0 +1,25 @@
+﻿// f(x) = 3*sin(sqrt(x)) + 0.35*x - 3.8
+// f'(x) = 3*cos(sqrt(x))*1/(2*sqrt(x))+0.35 
+// f''(x) = (3*sin(sqrt(x))*sqrt(x)+3*cos(sqrt(x)))/(4*x*sqrt(x))
+// [2; 3]
+
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    float x = 2;
+    float xprev = 0;
+    double eps = 0.000001;
+
+    while (abs(x - xprev) > eps)
+    {
+        xprev = x;
+        x = (3.8 - 3*sin(sqrt(xprev)))/0.35;
+    }
+    cout << x;
+    return 0;
+}
---
+++ Sem_1/nonlinear_equations/newton/newton.cpp
@@ -0,0 +1,41 @@
+﻿// f(x) = 3*sin(sqrt(x)) + 0.35*x - 3.8
+// f'(x) = 3*cos(sqrt(x))*1/(2*sqrt(x))+0.35 
+// f''(x) = (3*sin(sqrt(x))*sqrt(x)+3*cos(sqrt(x)))/(4*x*sqrt(x))
+// [2; 3]
+
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    float x;
+    float xprev = 0;
+    double eps = 0.000001;
+
+    if ((3 * sin(sqrt(3)) + 0.35 * 3 - 3.8) * ((3 * sin(sqrt(3)) * sqrt(3) + 3 * cos(sqrt(3))) / (4 * 3 * sqrt(3))) > 0)
+    {
+        x = 3;
+    }
+    else if ((3 * sin(sqrt(2)) + 0.35 * 2 - 3.8) * (3 * sin(sqrt(2)) * sqrt(2) + 3 * cos(sqrt(2))) / (4 * 2 * sqrt(2)) > 0)
+    {
+        x = 2;
+    }
+    else
+    {
+        cout << '-';
+        return 0;
+    }
+    
+    while (abs(x - xprev) > eps)
+    {
+        xprev = x;
+        x = xprev - (3 * sin(sqrt(xprev)) + 0.35 * xprev - 3.8) / (3 * cos(sqrt(xprev)) * 1 / (2 * sqrt(xprev)) + 0.35);
+    }
+
+    cout << x;
+
+    return 0;
+}
