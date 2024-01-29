3c06fd266dc76d05fcff2d61889c8247aaccbb6c
6o9puH <151661159+6o9puH@users.noreply.github.com>
2024-01-30 00:02:33 +0500

    Add files via upload

---
+++ Sem_2/3_1/3_1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	const int c = 10;
+	int a [c];
+
+	for (int i = 0; i < c; i++)
+	{
+		a[i] = rand() % 100;
+		cout << a[i] << endl;
+	}
+	cout << endl;
+
+	for (int i = 0; i < c / 2; i++)
+	{
+		int t = a[c - 1 - i];
+		a[c - 1 - i] = a[i];
+		a[i] = t;
+	}
+
+	for (int i = 0; i < c; i++)
+	{
+		cout << a[i] << endl;
+	}
+
+	return 0;
+}
---
+++ Sem_2/3_2/3_2.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	const int c = 10;
+	int p, q;
+
+	cin >> p >> q;
+
+	int a[c];
+
+	cout << endl;
+
+	for (int i = 0; i < c; i++)
+	{
+		a[i] = rand() % 100;
+		cout << a[i] << endl;
+	}
+
+	cout << endl;
+
+	while (p < q)
+	{
+		int t = a[p];
+		a[p] = a[q];
+		a[q] = t;
+		p++;
+		q--;
+	}
+
+	for (int i = 0; i < c; i++)
+	{
+		cout << a[i] << endl;
+	}
+
+	return 0;
+}
