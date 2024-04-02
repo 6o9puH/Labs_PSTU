fd067f7e9936d436e15b8965aa94c4d3f925dd92
6o9puH <151661159+6o9puH@users.noreply.github.com>
2024-04-02 22:21:46 +0500

    Add files via upload

---
+++ Sem_2/Labs/12sort/LomutoSort/LomutoSort.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int* createArray(int n, int max, int min)
+{
+	srand(time(0));
+	int* a = new int[n];
+
+	for (int i = 0; i < n; i++)
+	{
+		a[i] = rand() % max + min;
+
+		cout << a[i] << " ";
+	}
+
+	cout << endl << endl;
+
+	return a;
+}
+
+
+int partition(int* arr, int low, int high)
+{
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j <= high - 1; j++)
+    {
+        if (arr[j] <= pivot)
+        {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+
+    return (i + 1);
+}
+
+void quickSort(int* arr, int low, int high)
+{
+    if (low < high)
+    {
+        int pI = partition(arr, low, high);
+        quickSort(arr, low, pI - 1);
+        quickSort(arr, pI + 1, high);
+    }
+}
+
+int main()
+{
+	int n = 0, max = -1, min = -1;
+
+	while (n <= 0)
+	{
+		cout << "Enter number of elements" << endl;
+		cin >> n;
+	}
+
+	while (max < 0)
+	{
+		cout << "Enter maximum value (>= 0)" << endl;
+		cin >> max;
+	}
+
+	while (min < 0)
+	{
+		cout << "Enter minimum value (>= 0)" << endl;
+		cin >> min;
+	}
+
+	int* arr = createArray(n, max, min);
+	quickSort(arr, 0, n - 1);
+
+	cout << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+	return 0;
+}
